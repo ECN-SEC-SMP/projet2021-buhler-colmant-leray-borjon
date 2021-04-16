4759496041641af6ceb472a765c8888480885357
Louis Colmant <>
2021-04-16 14:59:49 +0000

    structure du code : Case; CaseFixe; Emplacement Construc

---
+++ EmplacementConstructible.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class EmplacementConstructible: public CaseAchetable
+{
+  private:
+    int batiment;
+    int coefValorisation;
+
+  protected:
+
+  public:
+    //constructeurs
+    EmplacementConstructible(int batiment, int coefValorisation);
+
+};
