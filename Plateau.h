028f2475b1df78f6d4451f4100944262479abb2e
Nolann Borjon-Piron <nolann.borjon@gmail.com>
2021-04-16 15:44:50 +0200

    Add plateau.h

---
+++ Plateau.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <stdlib.h>
+
+using namespace std;
+
+class Plateau {
+    private:
+        Case[40] plateau;
+        vector<Joueur> joueursActifs;
+        vector<Joueur> joueursInactifs;
+
+    public:
+        void initPlateau();
+        void affichage();
+        Case avance(Case case, int d);
+        void finDePartie();
+        int nbGares(Joueur j);
+};
